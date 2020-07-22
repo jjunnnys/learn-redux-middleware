dc2a97692ed018ce967669323afad72766692122
ppby <salswnss@gmail.com>
2020-07-22 11:17:44 +0900

    Modify thunk func refactoring

---
+++ src/lib/createRequestThunk.js
@@ -0,0 +1,25 @@
+export default function createRequestThunk(type, request) {
+  // 성공 및 실패 액션 타입을 정의한다.
+  const SUCCESS = `${type}_SUCCESS`;
+  const FAILURE = `${type}_FAILURE`;
+
+  return (params) => async (dispatch) => {
+    dispatch({ type }); // 시작
+    try {
+      const response = await request(params);
+      dispatch({
+        type: SUCCESS,
+        payload: response.data,
+      }); // 성공
+    } catch (error) {
+      dispatch({
+        type: FAILURE,
+        payload: error,
+        error: true,
+      }); // 에러 발생
+      throw error;
+    }
+  };
+}
+
+// 사용법 : createRequestThunk('GET_USERS',api.getUsers)
