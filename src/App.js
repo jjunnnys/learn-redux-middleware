ad12beffde1831fa335f28b7a6f9553cf0b8bd4d
ppby <salswnss@gmail.com>
2020-07-30 15:40:51 +0900

    Set sample module in rootsaga

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import CounterContainer from './containers/CounterContainer';
+import SampleContainer from './containers/SampleContainer';
 
 function App() {
   return (
     <div>
       <CounterContainer />
+      <SampleContainer />
     </div>
   );
 }
