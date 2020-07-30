083e7ad6ed77a73904a0ae254100cc42b1e9b852
ppby <salswnss@gmail.com>
2020-07-30 14:54:18 +0900

    Set redux-devtool-extension

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
-import rootReducer from './module';
+import rootReducer, { rootSaga } from './module';
 import { createLogger } from 'redux-logger';
 import ReduxThunk from 'redux-thunk';
 import createSagaMiddleware from 'redux-saga';
-import { rootSaga } from './module/index';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const logger = createLogger();
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
   rootReducer,
-  applyMiddleware(logger, ReduxThunk, sagaMiddleware)
+  composeWithDevTools(applyMiddleware(logger, ReduxThunk, sagaMiddleware))
 );
 
 sagaMiddleware.run(rootSaga);
